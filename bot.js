55174d80e07a4fe1018dac7c6fa98593bcffaa6f
alphanxd <alphanxd58@gmail.com>
2026-03-14 00:54:03 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,27 @@
 const mineflayer = require('mineflayer')
 const { pathfinder, Movements, goals } = require('mineflayer-pathfinder')
 const pvp = require('mineflayer-pvp').plugin
+const express = require('express')
 
-function startBot() {
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.get("/", (req, res) => {
+  res.send("Alpha Quantum Bot Running")
+})
+
+app.listen(PORT, () => {
+  console.log("Web server running")
+})
+
+function createBot() {
 
 const bot = mineflayer.createBot({
-host: "QuantumworldSMP.aternos.me",
-port: 15376,
-username: "AlphaBot"
+  host: "QuantumworldSMP.aternos.me",
+  port: 15376,
+  username: "ALPHA_Bot",
+  auth: "offline",
+  version: false
 })
 
 bot.loadPlugin(pathfinder)
@@ -15,132 +29,51 @@ bot.loadPlugin(pvp)
 
 bot.once('spawn', () => {
 
-console.log("AlphaBot joined the server")
+  console.log("🤖 Alpha Bot joined server")
 
-const mcData = require('minecraft-data')(bot.version)
-const movements = new Movements(bot, mcData)
+  const mcData = require('minecraft-data')(bot.version)
+  const defaultMove = new Movements(bot, mcData)
 
-bot.pathfinder.setMovements(movements)
+  bot.pathfinder.setMovements(defaultMove)
 
-bot.chat("I'm keeping the server alive!")
+  // follow you
+  setInterval(() => {
 
-startAntiAFK()
-startWandering()
-startMobCombat()
+    const player = bot.players["TheQuantxD"]
 
-})
+    if (!player || !player.entity) return
 
-function startAntiAFK() {
+    const goal = new goals.GoalFollow(player.entity, 2)
 
-setInterval(() => {
+    bot.pathfinder.setGoal(goal, true)
 
-const actions = ['forward','back','left','right','jump']
-const action = actions[Math.floor(Math.random()*actions.length)]
-
-bot.setControlState(action,true)
-
-setTimeout(() => {
-bot.setControlState(action,false)
-},2000)
-
-},15000)
-
-}
-
-function startWandering(){
-
-setInterval(() => {
-
-const x = bot.entity.position.x + (Math.random()*20-10)
-const y = bot.entity.position.y
-const z = bot.entity.position.z + (Math.random()*20-10)
-
-const goal = new goals.GoalNear(x,y,z,1)
-
-bot.pathfinder.setGoal(goal)
-
-},30000)
-
-}
-
-// Attack nearby hostile mobs
-function startMobCombat(){
-
-setInterval(()=>{
-
-const mob = bot.nearestEntity(entity =>
-entity.type === "mob"
-)
-
-if(mob){
-bot.pvp.attack(mob)
-}
-
-},5000)
-
-}
-
-// Fight players who hit the bot
-bot.on('entityHurt',(entity)=>{
-
-if(entity === bot.entity){
-
-const enemy = bot.nearestEntity(e =>
-e.type === "player" && e.username !== bot.username
-)
-
-if(enemy){
-console.log("Attacking",enemy.username)
-bot.chat("Why did you hit me?")
-bot.pvp.attack(enemy)
-}
-
-}
+  }, 5000)
 
 })
 
-// Chat commands
-bot.on('chat',(username,message)=>{
+bot.on('physicsTick', () => {
 
-if(username === bot.username) return
+  const mob = bot.nearestEntity(e => e.type === 'mob')
 
-const msg = message.toLowerCase()
+  if (!mob) return
 
-if(msg === "hello"){
-bot.chat("Hello "+username)
-}
-
-if(msg === "follow"){
-
-const player = bot.players[username]
-
-if(!player || !player.entity) return
-
-bot.chat("Following you")
-
-bot.pathfinder.setGoal(
-new goals.GoalFollow(player.entity,1),
-true
-)
-
-}
-
-if(msg === "stop"){
-bot.chat("Stopping")
-bot.pathfinder.setGoal(null)
-}
+  bot.pvp.attack(mob)
 
 })
 
-bot.on('end',()=>{
-console.log("Disconnected, reconnecting...")
-setTimeout(startBot,15000)
+bot.on("spawn", () => {
+  setTimeout(() => {
+    bot.chat("/login chalol78")
+  }, 3000)
 })
 
-bot.on('error',(err)=>{
-console.log(err)
+bot.on("end", () => {
+  console.log("Bot disconnected. Reconnecting in 10s...")
+  setTimeout(createBot, 10000)
 })
+
+bot.on("error", console.log)
 
 }
 
-startBot()
+createBot()
