41b5780c274b7db6ca25cd51312d67c92a8f4249
alphanxd <alphanxd58@gmail.com>
2026-03-13 15:43:56 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,140 +1,145 @@
 const mineflayer = require('mineflayer')
 const { pathfinder, Movements, goals } = require('mineflayer-pathfinder')
+const pvp = require('mineflayer-pvp').plugin
 
 function startBot() {
 
 const bot = mineflayer.createBot({
-  host: "QuantumworldSMP.aternos.me",
-  port: 15376,
-  username: "AlphaBot"
+host: "QuantumworldSMP.aternos.me",
+port: 15376,
+username: "AlphaBot"
 })
 
 bot.loadPlugin(pathfinder)
+bot.loadPlugin(pvp)
 
 bot.once('spawn', () => {
 
-  console.log("AlphaBot joined the server")
+console.log("AlphaBot joined the server")
 
-  const mcData = require('minecraft-data')(bot.version)
-  const movements = new Movements(bot, mcData)
-  bot.pathfinder.setMovements(movements)
+const mcData = require('minecraft-data')(bot.version)
+const movements = new Movements(bot, mcData)
 
-  // exploration wandering
-  setInterval(() => {
+bot.pathfinder.setMovements(movements)
 
-    const x = bot.entity.position.x + (Math.random()*20 - 10)
-    const z = bot.entity.position.z + (Math.random()*20 - 10)
-    const y = bot.entity.position.y
+bot.chat("I'm keeping the server alive!")
 
-    const goal = new goals.GoalNear(x, y, z, 1)
-    bot.pathfinder.setGoal(goal)
-
-  }, randomTime(20000,45000))
-
-  // human-like head movement
-  setInterval(() => {
-
-    const yaw = Math.random() * Math.PI * 2
-    const pitch = (Math.random()-0.5) * Math.PI/2
-
-    bot.look(yaw, pitch, true)
-
-  }, randomTime(6000,12000))
-
-  // anti AFK
-  setInterval(() => {
-
-    const actions = ['forward','back','left','right','jump']
-    const action = actions[Math.floor(Math.random()*actions.length)]
-
-    bot.setControlState(action,true)
-
-    setTimeout(()=>{
-      bot.setControlState(action,false)
-    }, randomTime(1000,2500))
-
-  }, randomTime(15000,30000))
+startAntiAFK()
+startWandering()
+startMobCombat()
 
 })
 
+function startAntiAFK() {
+
+setInterval(() => {
+
+const actions = ['forward','back','left','right','jump']
+const action = actions[Math.floor(Math.random()*actions.length)]
+
+bot.setControlState(action,true)
+
+setTimeout(() => {
+bot.setControlState(action,false)
+},2000)
+
+},15000)
+
+}
+
+function startWandering(){
+
+setInterval(() => {
+
+const x = bot.entity.position.x + (Math.random()*20-10)
+const y = bot.entity.position.y
+const z = bot.entity.position.z + (Math.random()*20-10)
+
+const goal = new goals.GoalNear(x,y,z,1)
+
+bot.pathfinder.setGoal(goal)
+
+},30000)
+
+}
+
+// Attack nearby hostile mobs
+function startMobCombat(){
+
+setInterval(()=>{
+
+const mob = bot.nearestEntity(entity =>
+entity.type === "mob"
+)
+
+if(mob){
+bot.pvp.attack(mob)
+}
+
+},5000)
+
+}
+
+// Fight players who hit the bot
+bot.on('entityHurt',(entity)=>{
+
+if(entity === bot.entity){
+
+const enemy = bot.nearestEntity(e =>
+e.type === "player" && e.username !== bot.username
+)
+
+if(enemy){
+console.log("Attacking",enemy.username)
+bot.chat("Why did you hit me?")
+bot.pvp.attack(enemy)
+}
+
+}
+
+})
+
+// Chat commands
 bot.on('chat',(username,message)=>{
 
-  if(username===bot.username) return
+if(username === bot.username) return
 
-  const msg = message.toLowerCase()
+const msg = message.toLowerCase()
 
-  if(msg==="hello"){
-    delayedChat("Hello "+username+"!")
-  }
+if(msg === "hello"){
+bot.chat("Hello "+username)
+}
 
-  if(msg==="alphabot"){
-    delayedChat("Yes? I'm here.")
-  }
+if(msg === "follow"){
 
-  if(msg==="follow"){
+const player = bot.players[username]
 
-    const player = bot.players[username]
+if(!player || !player.entity) return
 
-    if(!player || !player.entity) return
+bot.chat("Following you")
 
-    delayedChat("Okay, following you.")
-    bot.pathfinder.setGoal(new goals.GoalFollow(player.entity,1),true)
+bot.pathfinder.setGoal(
+new goals.GoalFollow(player.entity,1),
+true
+)
 
-  }
+}
 
-  if(msg==="stop"){
-    delayedChat("Stopping.")
-    bot.pathfinder.setGoal(null)
-  }
+if(msg === "stop"){
+bot.chat("Stopping")
+bot.pathfinder.setGoal(null)
+}
 
-  if(msg==="come"){
-
-    const player = bot.players[username]
-    if(!player || !player.entity) return
-
-    const p = player.entity.position
-
-    delayedChat("Coming.")
-    bot.pathfinder.setGoal(new goals.GoalNear(p.x,p.y,p.z,1))
-
-  }
-
-})
-
-bot.on('death',()=>{
-  console.log("AlphaBot died")
-})
-
-bot.on('kicked',(reason)=>{
-  console.log("Kicked:",reason)
 })
 
 bot.on('end',()=>{
-
-  console.log("Disconnected")
-
-  const delay = randomTime(15000,35000)
-
-  console.log("Reconnecting in "+delay/1000+" seconds")
-
-  setTimeout(()=>{
-    startBot()
-  },delay)
-
+console.log("Disconnected, reconnecting...")
+setTimeout(startBot,15000)
 })
 
-bot.on('error',err=>console.log(err))
-
-function delayedChat(text){
-  const delay = randomTime(1000,4000)
-  setTimeout(()=>{
-    bot.chat(text)
-  },delay)
-}
-
-function randomTime(min,max){
-  return Math.floor(Math.random()*(max-min)+min)
-}
+bot.on('error',(err)=>{
+console.log(err)
+})
 
 }
 
